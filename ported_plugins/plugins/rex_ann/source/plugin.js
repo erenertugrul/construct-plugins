2b88b21b56231a1eb19d0ff55010d59a0a5d7082
Eren Ertugrul <erenertugrul@gmail.com>
2019-07-28 12:08:14 +0300

    readme

---
+++ ported_plugins/plugins/rex_ann/source/plugin.js
@@ -0,0 +1,45 @@
+//Converted with C2C3AddonConverter v1.0.1.0
+"use strict";
+
+{
+	const PLUGIN_ID = "Rex_ANN";
+	const PLUGIN_VERSION = "0.1.0.0";
+	const PLUGIN_CATEGORY = "other";
+
+	let app = null;
+
+	const PLUGIN_CLASS = SDK.Plugins.Rex_ANN = class Rex_ANN extends SDK.IPluginBase
+	{
+		constructor()
+		{
+			super(PLUGIN_ID);
+			SDK.Lang.PushContext("plugins." + PLUGIN_ID.toLowerCase());
+			this._info.SetIcon("icon.png", "image/png");
+			this._info.SetName(lang(".name"));
+			this._info.SetDescription(lang(".description"));
+			this._info.SetVersion(PLUGIN_VERSION);
+			this._info.SetCategory(PLUGIN_CATEGORY);
+			this._info.SetAuthor("Rex.Rainbow");
+			this._info.SetHelpUrl(lang(".help-url"));
+			this._info.SetIsSingleGlobal(false);
+			this._info.SetIsDeprecated(false);
+			this._info.SetSupportsEffects(false);
+			this._info.SetMustPreDraw(false);
+			this._info.SetCanBeBundled(true);
+
+			this._info.SetSupportedRuntimes(["c2","c3"]);
+
+			SDK.Lang.PushContext(".properties");
+			this._info.SetProperties([
+				new SDK.PluginProperty("float", "rate", 0.5),
+				new SDK.PluginProperty("float", "momentum", 0.1),
+				new SDK.PluginProperty("text", "ınput-variables", ""),
+				new SDK.PluginProperty("integer", "hidden-layer", 3),
+				new SDK.PluginProperty("text", "output-variables", "")
+			]);
+			SDK.Lang.PopContext();		// .properties
+			SDK.Lang.PopContext();
+		}
+	};
+	PLUGIN_CLASS.Register(PLUGIN_ID, PLUGIN_CLASS);
+}
